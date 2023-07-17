16ca29ce03dd69917a778f37a5aac2627d7521a3
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2023-07-17 17:54:32 +0530

    Big O q

---
+++ DSA/BigO/Quesctions/Q1.java
@@ -0,0 +1,24 @@
+// package DSA.BigO.Quesctions;
+
+// // Time complexity = O(N)
+
+// class Sum{
+//     public void calculate(int[] arr){
+//         int sum = 0;
+//         int multipli = 1;
+//         for(int i=0; i<arr.length; i++){
+//             sum += arr[i];
+//             multipli *= arr[i];
+//         }
+//           System.out.println("sum: " + sum);
+//           System.out.println("Mul: " + multipli);
+//     }
+// }
+
+// public class Q1 {
+//     public static void main(String[] args) {
+//         int arr[] = {1,2,3,4,5,6,7};
+//         Sum sum = new Sum();
+//         sum.calculate(arr);
+//     }
+// }
---
+++ DSA/BigO/Quesctions/Q2.java
@@ -0,0 +1,22 @@
+package DSA.BigO.Quesctions;
+
+// Time complexity - O(N^2) 
+
+
+class PrintD{
+    public void doublePrint(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j= 0; j<arr.length; j++){
+                System.out.println(arr[i]+ "  " +arr[j]);
+            }
+        }
+    }
+}
+
+public class Q2 {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3};
+       PrintD p = new PrintD();
+       p.doublePrint(arr);
+    }
+}
