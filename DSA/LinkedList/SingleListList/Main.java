573eb257b14c32d38bdeea5dbde31ac45dec2531
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2023-07-19 18:04:07 +0530

    All sll methods

--- DSA/LinkedList/SingleListList/Main.java
+++ DSA/LinkedList/SingleListList/Main.java
@@ -9,14 +9,19 @@ package DSA.LinkedList.SingleListList;
 // Time complexity for creating a new LinkedList is O(1)
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SingleListList list = new SingleListList();
         list.createSingleLinkedList(5);
-        System.out.println(list.head.value); 
-        list.insertingNewNode(6,1);
-        System.out.println(list.head.next.value);
-        list.insertingNewNode(7,3);
-        System.out.println(list.head.next.next.value);
+        // System.out.println(list.head.value);
+        list.insertingNewNode(6, 1);
+        // System.out.println(list.head.next.value);
+        list.insertingNewNode(7, 3);
+        list.insertingNewNode(8, 0);
+        // System.out.println(list.head.next.next.value);
+        // list.traversalSingleList();
+        list.searching(7);
+        list.deleteOfNode(3);
+        list.deleteAllLists();
+        // list.traversalSingleList();
     }
 }
-
--- DSA/LinkedList/SingleListList/SingleListList.java
+++ DSA/LinkedList/SingleListList/SingleListList.java
@@ -2,7 +2,6 @@ package DSA.LinkedList.SingleListList;
 
 // Creating head and tail properties
 
-
 public class SingleListList {
     public Node head;
     public Node tail;
@@ -15,7 +14,7 @@ public class SingleListList {
         node.value = nodeValue;
         head = node;
         tail = node;
-        size =1;
+        size = 1;
         return head;
     }
 
@@ -24,20 +23,20 @@ public class SingleListList {
     public void insertingNewNode(int nodeValue, int location) {
         Node node = new Node();
         node.value = nodeValue;
-        if(head == null) {
+        if (head == null) {
             createSingleLinkedList(nodeValue);
             return;
-        } else if(location == 0) {
+        } else if (location == 0) {
             node.next = head;
             head = node;
-        } else if(location >= size) {
+        } else if (location >= size) {
             node.next = null;
             tail.next = node;
             tail = node;
         } else {
-        Node tempNode = head;
+            Node tempNode = head;
             int index = 0;
-            while(index < location -1)  {
+            while (index < location - 1) {
                 tempNode = tempNode.next;
                 index++;
             }
@@ -47,4 +46,74 @@ public class SingleListList {
         }
         size++;
     }
+
+    // Traversal of single list
+
+    public void traversalSingleList() {
+        Node tempNode = head;
+        for (int i = 0; i < size; i++) {
+            System.out.print(tempNode.value);
+            if (i != size - 1) {
+                System.out.print(" -> ");
+            }
+            tempNode = tempNode.next;
+        }
+        System.out.println("\n");
+    }
+
+    // Searching the value in the list
+    public void searching(int value) {
+        Node tempNode = head;
+        for (int i = 0; i < size; i++) {
+            if (tempNode.value == value) {
+                System.out.println("Value is located at location " + i);
+                return;
+            }
+            tempNode = tempNode.next;
+        }
+        System.out.println("Node not found");
+
+    }
+
+    // Deleting the node from first node, middle, last node.
+
+    public void deleteOfNode(int location) {
+        if (head == null) {
+            System.out.println("Node is empty"); //
+        } else if (location == 0) {
+            head = head.next;
+            size--;
+            if (size == 0) {
+                tail = null;
+            }
+        } else if (location >= size) {
+            Node tempNode = head;
+            for (int i = 0; i < size - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            if (tempNode == head) {
+                tail = head = null;
+                size--;
+                return;
+            }
+            tempNode.next = null;
+            tail = tempNode;
+            size--;
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < location - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = tempNode.next.next;
+            size--;
+        }
+    }
+
+    // Delete entire linked list
+    
+     public void deleteAllLists() {
+            head = null;
+            tail = null;
+            System.out.println("Success Delete All");
+     }
 }
